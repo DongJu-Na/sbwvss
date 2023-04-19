bf43d6cb252d23e73ec138f705afbe1084446517
DongJu-Na <ehdeld123@naver.com>
2023-04-19 18:59:01 +0900

    Update ApiController.java

--- src/main/java/com/sbwvss/controller/ApiController.java
+++ src/main/java/com/sbwvss/controller/ApiController.java
@@ -5,6 +5,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sbwvss.service.ApiService;
@@ -14,6 +15,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
+@RequestMapping("/api")
 @Slf4j
 @RestController
 public class ApiController {
@@ -22,7 +24,7 @@ public class ApiController {
 	ApiService service;
 	
 	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "get All Tweets") })
-  @GetMapping(value = "video/{title}", produces = "video/mp4")
+  @GetMapping(value = "/video/{title}", produces = "video/mp4")
   public Mono<Resource> getVideo(@PathVariable String title, @RequestHeader("Range") String range) {
       log.info("range > " + range);
       return service.getVideo(title);
